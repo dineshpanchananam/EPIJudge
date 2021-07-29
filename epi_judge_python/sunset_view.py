5d1967e3934409845f2c6bcbdc768868755a1222
dpanchananam <dpanchananam@bloomberg.net>
2021-07-29 12:17:52 -0400

    progress

--- epi_judge_python/sunset_view.py
+++ epi_judge_python/sunset_view.py
@@ -3,8 +3,12 @@ from typing import Iterator, List
 from test_framework import generic_test
 
 def examine_buildings_with_sunset(sequence: Iterator[int]) -> List[int]:
-  # TODO - you fill in here.
-  return []
+  can_see = []
+  for i, h in enumerate(sequence):
+    while can_see and h >= can_see[-1][-1]:
+      can_see.pop()
+    can_see.append([i, h])
+  return [x[0] for x in reversed(can_see)]
 
 def examine_buildings_with_sunset_wrapper(sequence):
   return examine_buildings_with_sunset(iter(sequence))
