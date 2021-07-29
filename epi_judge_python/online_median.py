dbbf75dfe27d9022de7e4965e28c144b1d61aaab
dpanchananam <dpanchananam@bloomberg.net>
2021-07-28 20:54:13 -0400

    shorter online median

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -3,49 +3,14 @@ from typing import Iterator, List
 from test_framework import generic_test
 import heapq as h
 
-def print_mx_heap(heap: 'MaxHeap'):
-  return sorted([-x for x in heap._max_h])
-
-class MaxHeap:
-  def __init__(self):
-    self._max_h = []
-    h.heapify(self._max_h)
-
-  def push(self, value):
-    h.heappush(self._max_h, -value)
-
-  def pop(self):
-    heap = self._max_h
-    if heap:
-      return -h.heappop(heap)
-
-  def max(self):
-    return -self._max_h[0]
-
-  def empty(self):
-    return self.len() == 0
-
-  def len(self):
-    return len(self._max_h)
-
 def online_median(sequence: Iterator[int]) -> List[float]:
-  mx_hp, mn_hp = MaxHeap(), []
+  mx_hp, mn_hp = [], []
   medians = []
   for n, s in enumerate(sequence, 1):
-    if not mn_hp or s >= mn_hp[0]:
-      h.heappush(mn_hp, s)
-    else:
-      mx_hp.push(s)
-
-    while not mx_hp.empty() and mn_hp and mn_hp[0] < mx_hp.max():
-      mx_hp.push(h.heappop(mn_hp))
-
-    if mx_hp.len() > len(mn_hp)+1:
-      h.heappush(mn_hp, mx_hp.pop())
-    elif len(mn_hp) > mx_hp.len():
-      mx_hp.push(h.heappop(mn_hp))
-
-    medians.append(mx_hp.max()/1.0 if n & 1 else (mx_hp.max()+mn_hp[0])/2.0)
+    h.heappush(mx_hp, -h.heappushpop(mn_hp, s))
+    if len(mx_hp) > len(mn_hp):
+      h.heappush(mn_hp, -h.heappop(mx_hp))
+    medians.append(mn_hp[0] if n & 1 else (mn_hp[0]-mx_hp[0])/2.0)
   return medians
 
   # print()
