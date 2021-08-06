45017c25e2f798eb905049fddda00e65d6df306b
dpanchananam <dpanchananam@bloomberg.net>
2021-08-06 17:32:55 -0400

    progress

--- epi_judge_python/left_right_justify_text.py
+++ epi_judge_python/left_right_justify_text.py
@@ -3,33 +3,30 @@ from typing import List
 from test_framework import generic_test
 
 def justify_text(words: List[str], L: int) -> List[str]:
-  def transform(x):
-    words = x.strip().split()
-    if len(words) == 1:
-      return f"{words[0]}{' '*(L-len(words[0]))}"
-    wl = sum(len(w) for w in words)
-    gaps = len(words)-1
-    spaces = (L-wl)//gaps
-    extra = L-wl-gaps*spaces
-    spaces = [" "*spaces]*gaps
-    for i in range(extra):
-      spaces[i] += " "
-    spaces.append('')
-    return "".join(w+s for (w, s) in zip(words, spaces))
-
-  res = []
-  i, n = 0, len(words)
-  tmp = ""
+  tmp, tl, res = [], 0, []
   for word in words:
-    if len(tmp+word) <= L:
-      tmp += word+" "
+    lw = len(word)
+    if tl+len(tmp)+lw <= L:
+      tmp.append(word)
+      tl += lw
     else:
-      res.append(transform(tmp))
-      tmp = word+" "
-  tmp = tmp.strip()
+      if len(tmp) == 1:
+        res.append(f'{tmp[0]}{" "*(L-tl)}')
+      else:
+        space, extra = divmod(L-tl, len(tmp)-1)
+        res.append((" "*space).join(tmp).replace(
+          " "*space,
+          " "*(space+1),
+          extra,
+        ))
+      tmp, tl = [word], lw
+
+  tmp = " ".join(tmp)
   return res+[tmp+' '*(L-len(tmp))]
 
 if __name__ == '__main__':
+  justify_text(
+    ["Text", "justification", "is", "trickier", "than", "it", "seems!"], 14)
   exit(
     generic_test.generic_test_main('left_right_justify_text.py',
                                    'left_right_justify_text.tsv',
